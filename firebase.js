57c1fe9164de08f31f0519e3b837d0516f16ea80
imanfsil <nurulimanfasil@gmail.com>
2026-08-02 18:30:22 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
-// Firebase Connection
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpuQJb4JBDbZC9bFWzVMLseji-qVNQMcI",
+  authDomain: "getwell-nutrition-coach.firebaseapp.com",
+  projectId: "getwell-nutrition-coach",
+  storageBucket: "getwell-nutrition-coach.firebasestorage.app",
+  messagingSenderId: "996763282056",
+  appId: "1:996763282056:web:219c4717188a3acbbc0d3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Make it available to other files
+window.firebaseApp = app;
+
+console.log("✅ Firebase Connected");
